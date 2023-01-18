ecf4f1d4e90880e55b9fb223c18545998940609c
jeckerma1998 <113375965+jeckerma1998@users.noreply.github.com>
2023-01-18 12:55:51 -0600

    HostPassword Summary
    changed settings.py file

--- homework/settings.py
+++ homework/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-137ip!bp*__3=hk$bl_o-s(29gx^97n7my01o&_&p(wclkgq%m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'homework.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'eckej158',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
